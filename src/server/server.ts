c5701f631fe6e2172bee3d9ff51161b714b08f2b
Shashi <shashikanthgadgay@gmail.com>
2020-10-14 23:40:48 +0530

    Revert "Add authorisation to colyseus monitoring"
    This reverts commit a753b10d960b4886b413c411efe9aad69247fa6b.

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,20 +3,11 @@ import express from "express";
 import cors from "cors";
 import {Server} from "colyseus";
 import {monitor} from "@colyseus/monitor";
-const basicAuth = require('express-basic-auth')
 
 import {CollabSketchRoom} from "./room/CollabSketchRoom";
-import * as process from "process";
+
 const port = Number(process.env.PORT || 2567);
-
-
-const app = express();
-
-// @ts-ignore
-const basicAuthMiddleware = basicAuth({
-    users: process.env.USERS,
-    challenge: true
-});
+const app = express()
 
 app.use(cors());
 app.use(express.json())
@@ -30,7 +21,7 @@ const gameServer = new Server({
 gameServer.define('collab-sketch', CollabSketchRoom);
 
 // register colyseus monitor AFTER registering your room handlers
-app.use("/colyseus", basicAuthMiddleware, monitor());
+app.use("/colyseus", monitor());
 
 gameServer.listen(port);
 console.log(`Listening on ws://localhost:${port}`)
